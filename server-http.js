0745e0df28071fedd8f12a217a31a3f8c4e69436
goofy-ace <c.nwike@crediblesimulations.com>
2025-12-14 13:23:18 +0000

    delete manual server

--- server-http.js
+++
@@ -1,7 +0,0 @@
-const http = require('http');
-
-http.createServer((req, res) => {
-  const host = req.headers['host'];
-  res.writeHead(301, { Location: 'https://' + host + req.url });
-  res.end();
-}).listen(80);
